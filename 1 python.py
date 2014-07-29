eaafd0973f4e5446d7fcc0bcba102629e21821ef
purpleberry <zoesw@penguinmail.com>
2014-07-29 13:32:06 +0100

    splitting the postcode and prise from the rest of the infomation

---
+++ 1 python.py
@@ -0,0 +1,13 @@
+import csv
+
+total = 0
+count = 0
+
+with open('pp-monthly-update-new-version.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in spamreader:
+        #print(row[1]+row[3])
+        total = total + int(row[1])
+        count = count + 1
+
+print(total/count)

b913d6c18e0c005882c1059dade30a09bc20e5d9
purpleberry <zoesw@penguinmail.com>
2014-07-30 09:44:22 +0100

    data is ready for map. Adding Police data

--- 1 python.py
+++ 1 python.py
@@ -6,8 +6,13 @@ count = 0
 with open('pp-monthly-update-new-version.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in spamreader:
-        #print(row[1]+row[3])
+        print(row[1]+row[3])
         total = total + int(row[1])
         count = count + 1
 
 print(total/count)
+
+str1 = "RG1 9LO"
+
+pos = str1.find(" ")
+print(str1[:pos])
